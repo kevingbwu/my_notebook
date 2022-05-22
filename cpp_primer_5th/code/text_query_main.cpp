e5c451f892616ee952622238235bcc6ce1c3d0fc
kevinWu <kevingbwu@gmail.com>
2022-05-22 22:05:21 +0800

    update

--- cpp_primer_5th/code/text_query_main.cpp
+++ cpp_primer_5th/code/text_query_main.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+// return the plural version of word if ctr is greater than 1
+string make_plural(size_t ctr, const string &word,
+                   const string &ending)
+{
+    return (ctr > 1) ? word + ending : word;
+}
+
+ostream &print(ostream & os, const QueryResult &qr)
+{
+    // if the word was found, print the count and all occurrences
+    os << qr.sought << " occurs " << qr.lines->size() << " "
+       << make_plural(qr.lines->size(), "time", "s") <<
+       endl;
+    // print each line in which the word appeared
+    for (auto num : *qr.lines) // for every element in the set
+        // don't confound the user with text lines starting at 0
+        os << "\t(line " << num + 1 << ") "
+           << *(qr.file->begin() + num) << endl;
+    return os;
+}
+
 class QueryResult {
     friend std::ostream& print(std::ostream&, const QueryResult&);
     using line_no = std::vector<std::string>::size_type;
@@ -58,27 +79,6 @@ QueryResult TextQuery::query(const string &sought) const
         return QueryResult(sought, loc->second, file);
 }
 
-// return the plural version of word if ctr is greater than 1
-string make_plural(size_t ctr, const string &word,
-                   const string &ending)
-{
-    return (ctr > 1) ? word + ending : word;
-}
-
-ostream &print(ostream & os, const QueryResult &qr)
-{
-    // if the word was found, print the count and all occurrences
-    os << qr.sought << " occurs " << qr.lines->size() << " "
-       << make_plural(qr.lines->size(), "time", "s") <<
-       endl;
-    // print each line in which the word appeared
-    for (auto num : *qr.lines) // for every element in the set
-        // don't confound the user with text lines starting at 0
-        os << "\t(line " << num + 1 << ") "
-           << *(qr.file->begin() + num) << endl;
-    return os;
-}
-
 void runQueries(ifstream &infile) {
     // infile is an ifstream that is the file we want to query
     TextQuery tq(infile); // store the file and build the query map
@@ -92,6 +92,10 @@ void runQueries(ifstream &infile) {
         print(cout, tq.query(s)) << endl;
     }
 }
+
+// test.txt:
+// A set element contains only a key;
+// operator creates a new element
 
 int main() {
     ifstream infile("test.txt");
